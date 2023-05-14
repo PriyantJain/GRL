e8cca5f4addf16c8f09bf30c02fa8d40d36b389e
PriyantJain <jainpriyantpankaj@gmail.com>
2023-05-14 12:43:52 +0530

    v4

--- GRL_app/model/mysql_writer.py
+++ GRL_app/model/mysql_writer.py
@@ -12,28 +12,32 @@ class BackendWriter:
         self.logs = []
         self.queries = []
 
+    def check_connection(self):
+        if self.connection.open == 1 : self.connection.close()
+        self.connection = MySQLdb.connect(host=host, user=user, password=password, database=database)
+
     def update_score(self, old_score, new_score) : self.queries.append(f'''UPDATE STATS SET Score = '{new_score}' WHERE Id = 1;''')
-    
+
     def update_last_open_date(self, _today) : self.queries.append(f'''UPDATE STATS SET Last_Open = '{_today}' WHERE Id = 1;''')
-    
+
     def update_last_day_score(self, last_score) : self.queries.append(f'''UPDATE STATS SET Last_Day_Score = '{last_score}' WHERE Id = 1;''')
-    
+
     def add_daily_task(self, t_no, task) :    self.queries.append(f'''UPDATE STATS SET DT_{t_no} = '{task}' WHERE Id = 1;''')
 
-    def delete_daily_task(self, default_message) : 
-        self.queries.append(f'''UPDATE STATS 
-                                SET DT_1 = '{default_message}', DT_2 = '{default_message}', DT_3 = '{default_message}', 
+    def delete_daily_task(self, default_message) :
+        self.queries.append(f'''UPDATE STATS
+                                SET DT_1 = '{default_message}', DT_2 = '{default_message}', DT_3 = '{default_message}',
                                 DT_1_completed = 0, DT_2_completed = 0, DT_3_completed = 0
                                 WHERE Id = 1;''')
-    
+
     def toggle_dt(self, t_no, state) :  self.queries.append(f'''UPDATE STATS SET DT_{t_no}_completed = '{state}' WHERE Id = 1;''')
 
     def add_to_do(self, task) : self.queries.append(f'''INSERT INTO TO_DO_TASKS(Task_Name, Completion_Date) VALUES('{task}', -1);''')
 
-    def complete_to_do(self, t_no, _today, points) : 
+    def complete_to_do(self, t_no, _today, points) :
         self.queries.append(f'''UPDATE TO_DO_TASKS SET Completion_Date = '{_today}', Task_Points = '{points}' WHERE Sr_No = {t_no};''')
 
-    def undo_to_do(self, t_no) : 
+    def undo_to_do(self, t_no) :
         self.queries.append(f'''UPDATE TO_DO_TASKS SET Completion_Date = '-1' WHERE Sr_No = {t_no};''')
 
     def delete_to_do(self, t_no) :    self.queries.append(f'''DELETE FROM TO_DO_TASKS WHERE Sr_No = {t_no};''')
@@ -41,13 +45,13 @@ class BackendWriter:
     def update_to_do(self, t_no, new_name) :
         self.queries.append(f'''UPDATE TO_DO_TASKS SET Task_Name = '{new_name}' WHERE Sr_No = {t_no};''')
 
-    def add_recurring_task(self, task, pts) : 
+    def add_recurring_task(self, task, pts) :
         self.queries.append(f'''INSERT INTO RECURRING_TASKS(Task_Name, Last_Completion_Date, Task_Points) VALUES('{task}', '0', '{pts}');''')
 
-    def complete_recurring_task(self, t_no, _today) : 
+    def complete_recurring_task(self, t_no, _today) :
         self.queries.append(f'''UPDATE RECURRING_TASKS SET Last_Completion_Date = '{_today}' WHERE Sr_No = {t_no};''')
 
-    def undo_recurring_task(self, t_no) : 
+    def undo_recurring_task(self, t_no) :
         self.queries.append(f'''UPDATE RECURRING_TASKS SET Last_Completion_Date = '0' WHERE Sr_No = {t_no};''')
 
     def delete_recurring_task(self, t_no) :    self.queries.append(f'''DELETE FROM RECURRING_TASKS WHERE Sr_No = {t_no};''')
@@ -58,6 +62,7 @@ class BackendWriter:
     def commit(self) :
         if len(self.add_log) == 0 : return       # if there's no log to push, return
 
+        self.check_connection()
         self.connection.begin()
         cursor = self.connection.cursor()
 
@@ -74,6 +79,7 @@ class BackendWriter:
 
 
     def get_table_data(self, table_name) :
+        self.check_connection()
         cursor = self.connection.cursor()
         cursor.execute(f'SELECT * FROM {table_name}')
         col_names = tuple(desc[0] for desc in cursor.description)
@@ -81,7 +87,7 @@ class BackendWriter:
 
     def pull_status(self, _today):
         variables = dict([])
-        
+
         for col, val in zip(*self.get_table_data('STATS')) :
             variables[col] = val
 
@@ -93,4 +99,6 @@ class BackendWriter:
 
         return variables
 
-    def __del__(self):    self.connection.close()
+    def __del__(self):
+        print('Closing connection via del..')
+        self.connection.close()
